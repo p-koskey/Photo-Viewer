f2142bf614c67be08e107c8ff0568ee99d479115
p-koskey <patiencekoskey@gmail.com>
2020-10-11 15:31:42 +0300

    update file structure and create path for homepage

---
+++ Makefile
@@ -0,0 +1,20 @@
+serve:
+	python manage.py runserver
+	
+superuser:
+	python manage.py createsuperuser --username $(name) 
+
+migrate:
+	python manage.py migrate
+	 
+app:
+	django-admin startapp $(name)
+
+migrations:
+	python manage.py makemigrations $(app)
+
+check:
+	python manage.py check
+	
+collectstatic:
+	python manage.py collectstatic
--- photoview/settings.py
+++ photoview/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import cloudinary
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photos',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,10 @@ WSGI_APPLICATION = 'photoview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'kaycee',
+    'PASSWORD':'password',
     }
 }
 
@@ -118,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+TIME_ZONE = 'Africa/Nairobi'
+
