afec6c77a39fa88e89cce981ff0faf3b2001965d
p-koskey <patiencekoskey@gmail.com>
2020-10-11 15:53:23 +0300

    create models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length =60)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    cityname = models.CharField(max_length =60)
+
+    def __str__(self):
+        return self.cityname
+
+class Photos(models.Model):
+    name = models.CharField(max_length =60)
+    caption = models.CharField(max_length =60)
+    categorytags = models.ManyToManyField(Category)
+    locationtags = models.ManyToManyField(Location)
+    image = CloudinaryField('image')
+
+    def __str__(self):
+        return self.name
