fd0e3e605db333c5e596860b8206a5cfd7ec895b
p-koskey <patiencekoskey@gmail.com>
2020-10-11 16:02:20 +0300

    display images in index.html

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from .models import Photos
 # Create your views here.
 def welcome(request):
-     return render(request, 'index.html')
+    images = Photos.objects.all() 
+    return render (request,'index.html',{'images':images})
